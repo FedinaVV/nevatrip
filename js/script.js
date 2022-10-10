15d88ebb3c6a6d20671957d45eefc25f3eccb2c9
FedinaVV <lerochka_2009@mail.ru>
2022-10-10 17:50:14 +0700

    fixed script

--- js/script.js
+++ js/script.js
@@ -6,13 +6,12 @@ function readMore() {
     for (let i = 0; i < moreTime.length; i++) {
         moreTime[i].classList.add('open');
 
-        if (window.innerWidth < 445) {
+        if (window.innerWidth < 445 && window.innerWidth > 373) {
 
             moreTime[0].style.marginRight = 0;
 
             for (let i = 4; i < moreTime.length; i += 4) {
                 moreTime[i].style.marginRight = 0;
-
             }
         }
     }
